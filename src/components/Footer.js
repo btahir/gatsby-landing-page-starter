23bbb7df377788ae7a5b45413d2f6495934e68ad
btahir <bilal.tahir89@gmail.com>
2020-05-17 12:15:20 -0700

    increase social media icons size

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,9 +9,9 @@ const Footer =  ({ siteTitle }) => {
         &copy; 2020 {siteTitle}
       </div>
         <div className="social-media">
-          <a href="https://www.facebook.com"><div className="social-facebook"></div></a>
-          <a href="https://www.twitter.com"><div className="social-twitter"></div></a>
-          <a href="https://www.instagram.com"><div className="social-instagram"></div></a>
+          <a href="https://www.facebook.com"><div className="social-facebook">Facebook</div></a>
+          <a href="https://www.twitter.com"><div className="social-twitter">Twitter</div></a>
+          <a href="https://www.instagram.com"><div className="social-instagram">Instagram</div></a>
         </div>
       </div>
     </footer>
